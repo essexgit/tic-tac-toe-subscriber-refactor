aad3a66e8f3ec744dde4214fa4027b7209200a37
Ralph Cox <ralphsmail@btinternet.com>
2024-03-17 17:32:05 +0000

    up to local storage

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -1,125 +1,128 @@
 import View from './view.js'
 import Store from './store.js'
-const App = {
-  $: {
-    menu: document.querySelector('[data-id="menu"]'),
-    menuItems: document.querySelector('[data-id="menu-items"]'),
-    resetBtn: document.querySelector('[data-id="reset-btn"]'),
-    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
-    squares: document.querySelectorAll('[data-id="square"]'),
-    modal: document.querySelector('[data-id="modal"]'),
-    modalText: document.querySelector('[data-id="modal-text"]'),
-    modalBtn: document.querySelector('[data-id="modal-btn"]'),
-    turn: document.querySelector('[data-id="turn"]'),
-  },
-  state: {
-    moves: [],
-  },
+// const App = {
+//   $: {
+//     menu: document.querySelector('[data-id="menu"]'),
+//     menuItems: document.querySelector('[data-id="menu-items"]'),
+//     resetBtn: document.querySelector('[data-id="reset-btn"]'),
+//     newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+//     squares: document.querySelectorAll('[data-id="square"]'),
+//     modal: document.querySelector('[data-id="modal"]'),
+//     modalText: document.querySelector('[data-id="modal-text"]'),
+//     modalBtn: document.querySelector('[data-id="modal-btn"]'),
+//     turn: document.querySelector('[data-id="turn"]'),
+//     p1Wins: document.querySelector('[data-id="p1-wins]'),
+//     p2Wins: document.querySelector('[data-id="p2-wins]'),
+//     ties: document.querySelector('[data-id="ties]')
+//   },
+//   state: {
+//     moves: [],
+//   },
 
-  getGameStatus (moves) {
-    const p1Moves = moves.filter(move => move.playerId === 1).map(move => +move.squareId)
-    const p2Moves = moves.filter(move => move.playerId === 2).map(move => +move.squareId)
-    const winningPatterns = [
-      [1, 2, 3],
-      [1, 5, 9],
-      [1, 4, 7],
-      [2, 5, 8],
-      [3, 5, 7],
-      [3, 6, 9],
-      [4, 5, 6],
-      [7, 8, 9],
-    ];
-    let winner = null;
+//   getGameStatus (moves) {
+//     const p1Moves = moves.filter(move => move.playerId === 1).map(move => +move.squareId)
+//     const p2Moves = moves.filter(move => move.playerId === 2).map(move => +move.squareId)
+//     const winningPatterns = [
+//       [1, 2, 3],
+//       [1, 5, 9],
+//       [1, 4, 7],
+//       [2, 5, 8],
+//       [3, 5, 7],
+//       [3, 6, 9],
+//       [4, 5, 6],
+//       [7, 8, 9],
+//     ];
+//     let winner = null;
 
-    winningPatterns.forEach(pattern => {
-      const p1Wins = pattern.every(v => p1Moves.includes(v))
-      const p2Wins = pattern.every(v => p2Moves.includes(v))
+//     winningPatterns.forEach(pattern => {
+//       const p1Wins = pattern.every(v => p1Moves.includes(v))
+//       const p2Wins = pattern.every(v => p2Moves.includes(v))
 
-      if (p1Wins) winner = 1
-      if (p2Wins) winner = 2
-    })
-    return {
-      status: moves.length === 9 || winner !== null ? 'complete' : 'in-progress',
-      winner // 1 | 2 | null
-    }
-  },
+//       if (p1Wins) winner = 1
+//       if (p2Wins) winner = 2
+//     })
+//     return {
+//       status: moves.length === 9 || winner !== null ? 'complete' : 'in-progress',
+//       winner // 1 | 2 | null
+//     }
+//   },
 
-  init () {
-    App.registerEventListeners()
-  },
+//   init () {
+//     App.registerEventListeners()
+//   },
 
-  registerEventListeners () {
-    App.$.menu.addEventListener('click', event => {
-      App.$.menuItems.classList.toggle('hidden')
-    })
-    App.$.resetBtn.addEventListener('click', (e) => {
-      console.log(e.target)
-    })
-    App.$.newRoundBtn.addEventListener('click', (e) => {
-      console.log(e.target)
-    })
-    App.$.modalBtn.addEventListener('click', (e) => {
-      App.state.moves = []
-      App.$.squares.forEach(square => square.replaceChildren())
-      App.$.modal.classList.add('hidden')
-    })
+//   registerEventListeners () {
+//     App.$.menu.addEventListener('click', event => {
+//       App.$.menuItems.classList.toggle('hidden')
+//     })
+//     App.$.resetBtn.addEventListener('click', (e) => {
+//       console.log(e.target)
+//     })
+//     App.$.newRoundBtn.addEventListener('click', (e) => {
+//       console.log(e.target)
+//     })
+//     App.$.modalBtn.addEventListener('click', (e) => {
+//       App.state.moves = []
+//       App.$.squares.forEach(square => square.replaceChildren())
+//       App.$.modal.classList.add('hidden')
+//     })
 
-    App.$.squares.forEach((square) => {
-      square.addEventListener('click', (event) => {
-        // check if there is already a play, then return early
-        const hasMove = (squareId) => {
-          const existingMove = App.state.moves.find(move => move.squareId === squareId)
-          return existingMove !== undefined
-        }
-        if (hasMove(+square.id)) {
-          return
-        }
+//     App.$.squares.forEach((square) => {
+//       square.addEventListener('click', (event) => {
+//         // check if there is already a play, then return early
+//         const hasMove = (squareId) => {
+//           const existingMove = App.state.moves.find(move => move.squareId === squareId)
+//           return existingMove !== undefined
+//         }
+//         if (hasMove(+square.id)) {
+//           return
+//         }
 
-        // Determine which player icon to add to the square
-        const lastMove = App.state.moves.at(-1)
-        const getOppositePlayer = (playerId) => playerId === 1 ? 2 : 1
-        const currentPlayer =
-          App.state.moves.length === 0
-            ? 1
-            : getOppositePlayer(lastMove.playerId)
-        const nextPlayer = getOppositePlayer(currentPlayer)
-        const squareIcon = document.createElement('i')
-        const turnIcon = document.createElement('i')
-        const turnLabel = document.createElement('p')
-        turnLabel.innerText = `Player ${nextPlayer} you are up!`
+//         // Determine which player icon to add to the square
+//         const lastMove = App.state.moves.at(-1)
+//         const getOppositePlayer = (playerId) => playerId === 1 ? 2 : 1
+//         const currentPlayer =
+//           App.state.moves.length === 0
+//             ? 1
+//             : getOppositePlayer(lastMove.playerId)
+//         const nextPlayer = getOppositePlayer(currentPlayer)
+//         const squareIcon = document.createElement('i')
+//         const turnIcon = document.createElement('i')
+//         const turnLabel = document.createElement('p')
+//         turnLabel.innerText = `Player ${nextPlayer} you are up!`
 
-        if (currentPlayer === 1) {
-          squareIcon.classList.add('fa-solid', 'fa-x', 'turquoise')
-          turnIcon.classList.add('fa-solid', 'fa-o', 'yellow')
-          turnLabel.classList.add('yellow')
-        } else {
-          squareIcon.classList.add('fa-solid', 'fa-o', 'yellow')
-          turnIcon.classList.add('fa-solid', 'fa-x', 'turquoise')
-          turnLabel.classList.add('turquoise')
-        }
-        square.replaceChildren(squareIcon)
+//         if (currentPlayer === 1) {
+//           squareIcon.classList.add('fa-solid', 'fa-x', 'turquoise')
+//           turnIcon.classList.add('fa-solid', 'fa-o', 'yellow')
+//           turnLabel.classList.add('yellow')
+//         } else {
+//           squareIcon.classList.add('fa-solid', 'fa-o', 'yellow')
+//           turnIcon.classList.add('fa-solid', 'fa-x', 'turquoise')
+//           turnLabel.classList.add('turquoise')
+//         }
+//         square.replaceChildren(squareIcon)
 
-        App.$.turn.replaceChildren(turnIcon, turnLabel)
-        App.state.moves.push({
-          squareId: +square.id,
-          playerId: currentPlayer
-        })
-        //check if there  is a winner or a tie game
-        const game = App.getGameStatus(App.state.moves)
-        if (game.status === 'complete') {
-          App.$.modal.classList.remove('hidden')
-          let message = ''
-          if (game.winner) {
-            message = `Player ${game.winner.name} wins!`
-          } else {
-            message = `It's a tie!`
-          }
-          App.$.modalText.textContent = message
-        }
-      })
-    })
-  },
-}
+//         App.$.turn.replaceChildren(turnIcon, turnLabel)
+//         App.state.moves.push({
+//           squareId: +square.id,
+//           playerId: currentPlayer
+//         })
+//         //check if there  is a winner or a tie game
+//         const game = App.getGameStatus(App.state.moves)
+//         if (game.status === 'complete') {
+//           App.$.modal.classList.remove('hidden')
+//           let message = ''
+//           if (game.winner) {
+//             message = `Player ${game.winner.name} wins!`
+//           } else {
+//             message = `It's a tie!`
+//           }
+//           App.$.modalText.textContent = message
+//         }
+//       })
+//     })
+//   },
+// }
 const players = [
   {
     id: 1,
@@ -138,14 +141,20 @@ const players = [
 
 function init () {
   const view = new View()
-  const store = new Store(players)
-  view.bindGameResetEvent(event => {
-    console.log("Reset Game ")
-    console.log(event.target)
+  const store = new Store('live-t3-storage-key', players)
+  view.bindGameResetEvent((event) => {
+    view.closeAll()
+    store.reset()
+    view.clearMoves()
+    view.setTurnIndicator(store.game.currentPlayer)
+    view.updateScoreboard(store.stats.playerWithStats[0].wins, store.stats.playerWithStats[1].wins, store.stats.ties)
   })
-  view.bindNewRoundEvent(event => {
-    console.log("New Round")
-    console.log(event.target)
+  view.bindNewRoundEvent((event) => {
+    store.newRound()
+    view.closeAll()
+    view.clearMoves()
+    view.setTurnIndicator(store.game.currentPlayer)
+    view.updateScoreboard(store.stats.playerWithStats[0].wins, store.stats.playerWithStats[1].wins, store.stats.ties)
   })
   view.bindPlayerMoveEvent((square) => {
 
--- live-vanilla-build/js/store.js
+++ live-vanilla-build/js/store.js
@@ -1,5 +1,9 @@
 const initialValue = {
-  moves: []
+  currentGameMoves: [],
+  history: {
+    currentRoundGames: [],
+    allGames: []
+  }
 }
 
 export default class Store {
@@ -8,9 +12,24 @@ export default class Store {
     this.players = players
   }
 
+  get stats () {
+
+    const state = this.#getState()
+    return {
+      playerWithStats: this.players.map((player) => {
+        const wins = state.history.currentRoundGames.filter((game) => game.status.winner?.id === player.id).length;
+        return {
+          ...player,
+          wins
+        }
+      }),
+      ties: state.history.currentRoundGames.filter((game) => game.status.winner === null).length
+    }
+  }
+
   get game () {
     const state = this.#getState()
-    const currentPlayer = this.players[state.moves.length % 2]
+    const currentPlayer = this.players[state.currentGameMoves.length % 2]
     const winningPatterns = [
       [1, 2, 3],
       [1, 5, 9],
@@ -23,7 +42,7 @@ export default class Store {
     ];
     let winner = null;
     for (const player of this.players) {
-      const selectedSquareIds = state.moves.filter(
+      const selectedSquareIds = state.currentGameMoves.filter(
         (move) => move.player.id === player.id
       ).map(move => move.squareId)
       for (const pattern of winningPatterns) {
@@ -33,22 +52,41 @@ export default class Store {
       }
     }
     return {
-      moves: state.moves,
+      moves: state.currentGameMoves,
       currentPlayer,
       status: {
-        isComplete: winner != null || state.moves.length === 9,
+        isComplete: winner != null || state.currentGameMoves.length === 9,
         winner,
       }
     }
   }
   playerMove (squareId) {
-    const state = this.#getState()
+    const stateClone = structuredClone(this.#getState())
 
-    const stateClone = structuredClone(state)
-    stateClone.moves.push({
+    stateClone.currentGameMoves.push({
       squareId,
       player: this.game.currentPlayer
     })
+    this.#saveState(stateClone)
+  }
+  reset () {
+    const stateClone = structuredClone(this.#getState())
+
+    const { status, moves } = this.game
+    if (status.isComplete) {
+      stateClone.history.currentRoundGames.push({
+        moves,
+        status
+      })
+    }
+    stateClone.currentGameMoves = []
+    this.#saveState(stateClone)
+  }
+  newRound () {
+    this.reset()
+    const stateClone = structuredClone(this.#getState())
+    stateClone.history.allGames.push(...stateClone.history.currentRoundGames)
+    stateClone.history.currentRoundGames = []
     this.#saveState(stateClone)
   }
   #getState () {
